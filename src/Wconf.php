d8460c7d0cbfc0ffa710ec564d0e591257ab1bf1
wodrow-ys-pc <wodrow451611cv@gmail.com>
2019-04-18 09:16:21 +0800

    init

--- src/Wconf.php
+++ src/Wconf.php
@@ -13,9 +13,47 @@ use wodrow\yii2wconf\models\WwConfig;
 
 class Wconf extends WwConfig
 {
-    const VT_INT = 1;
-    const VT_STR = 2;
+    const VT_STR = 1;
+    const VT_INT = 2;
     const VT_TEXT = 3;
 
     const STATUS_ACTIVE = 10;
+    const STATUS_DEL = -10;
+
+    public static function set($k, $v, $group = '')
+    {
+        $m = self::findOne(['k' => $k, 'group' => $group]);
+        if (!$m){
+            $m = new static();
+            $m->k = $k;
+            $m->group = $group;
+            $m->v_type = self::VT_STR;
+        }
+        $m->status = self::STATUS_ACTIVE;
+        $m->v = $v;
+        return $m->save();
+    }
+
+    public static function get($k, $group = '')
+    {
+        $m = self::findOne(['k' => $k, 'group' => $group]);
+        if (!$m){
+            return null;
+        }
+        if ($m->status != self::STATUS_ACTIVE){
+            return null;
+        }
+        return $m->v;
+    }
+
+    public static function del($k, $group = '')
+    {
+        $m = self::findOne(['k' => $k, 'group' => $group]);
+        if ($m){
+            $m->status = self::STATUS_DEL;
+            return $m->save();
+        }else{
+            return true;
+        }
+    }
 }
